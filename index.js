515b7a5eef88b6e2c0c1c5d636434655b5087750
Anyang Diing <anyang.ngor@student.moringaschool.com>
2024-12-24 10:49:06 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// Variables for the multiply test
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 31 * 2 = 62
 
-// Write your code here
+// Variable for the random test
+const random = Math.floor(Math.random() * 100) + 1; // Random integer greater than 0
+
+// Variables for the mod test
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4; // 10 % 6 = 4
+
+// Variable for the max test
+const max = Math.max(1, 5, 10, 20); // Returns 20
+
+// Export variables (if required)
+module.exports = { num1, num2, multiply, random, num3, num4, mod, max };
